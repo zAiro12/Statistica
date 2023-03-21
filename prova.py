0a7267367b297bd6e95104267bd3aad5f49bbae5
zAiro12 <lucaairoldi92@gmail.com>
2023-03-21 11:03:17 +0100

    add lesson

---
+++ prova.py
@@ -0,0 +1,18 @@
+def ciao():
+    c = "variabile ciao"
+    print("ciao funzione")
+    return c 
+
+lambda s: len(s)
+ciao
+
+v = ["ciao", "ciao", "hhhhh"]
+
+tupla = (ciao(), v)
+
+tupla[1].append("greg")
+
+print(tupla)
+
+for c in "camomilla":
+    print(c, end='*')
